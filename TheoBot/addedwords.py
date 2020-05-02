1c63013ed782373ec33277d9d0c5357812366088
Jack Sanders <jacksanders404@gmail.com>
2020-05-02 17:38:32 +0100

    Added extra words + changed method for adding words
    Words added:
    Plagueis
    Midichlorians
    Sith
    
    Added a second .py file containing addedWords dict, which is merged with cmuDict when the program is run, to prevent uploading ~14k lines every time it gets updated.

---
+++ TheoBot/addedwords.py
@@ -0,0 +1,6 @@
+addedWords = {
+    "ANIME" : ["AE1", "N", "AH0", "M", "EY2"],
+    "MIDICHLORIANS" : ["M", "IH1", "D", "IY1", "K", "L", "AO1", "R", "IY1", "AE1", "N", "S"],
+    "PLAGUEIS" : ["P", "L", "EY1", "G", "IH1", "S"],
+    "SITH" : ["S", "IH1", "TH"]
+}
